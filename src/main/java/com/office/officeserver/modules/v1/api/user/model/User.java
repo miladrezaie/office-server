a70a0a1f147f9dc06f989f77d6fa84a1d8d40dae
milad rezaie <m.rezai2724@gmail.com>
2021-09-22 04:36:28 -0400

    milad

--- src/main/java/com/office/officeserver/modules/v1/api/user/model/User.java
+++ src/main/java/com/office/officeserver/modules/v1/api/user/model/User.java
@@ -2,24 +2,28 @@ package com.office.officeserver.modules.v1.api.user.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.office.officeserver.modules.v1.api.employee.model.Employee;
+import com.office.officeserver.modules.v1.api.role.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.validator.constraints.Length;
 
-
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.*;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "personal_id"),
+        @UniqueConstraint(columnNames = "email")
+})
 @Getter
 @Setter
 @AllArgsConstructor
@@ -27,13 +31,13 @@ import java.util.List;
 
 public class User {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
 
     @Column(name = "personal_id", columnDefinition = "nvarchar(10)", unique = true)
+    @NotBlank
     private String personal_id;
 
 
@@ -41,20 +45,22 @@ public class User {
     private String first_name;
 
 
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+
+    @NotBlank
+    @Pattern(regexp = "(^$|[0-9]{10})")
+    private String mobile;
+
     @Column(name = "last_name", columnDefinition = "nvarchar(20)")
     private String Last_name;
 
 
-    @Column(columnDefinition = "nvarchar(25)")
-    private String full_name;
-
-
     @Column(columnDefinition = "nvarchar(10)")
     private String finger;
-
-
-    @Column(columnDefinition = "nvarchar(20)")
-    private String job;
 
 
     @Column(name = "active")
@@ -78,7 +84,9 @@ public class User {
 
     @Column(name = "password")
     @JsonIgnore
-    private String pass;
+    @NotBlank
+    @Size(max = 120)
+    private String password;
 
 
     @OneToMany(cascade = CascadeType.ALL)
@@ -86,15 +94,11 @@ public class User {
     private List<Employee> employees;
 
 
-//    @ManyToMany(cascade = {CascadeType.DETACH,
-//            CascadeType.MERGE,
-//            CascadeType.REFRESH,
-//            CascadeType.PERSIST})
-//    @JoinTable(
-//            name = "role_user",
-//            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-//            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"),
-//            uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role_id"})})
-////    @JsonIgnoreProperties("employees")
-//    private List<Role> roles;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
 }
